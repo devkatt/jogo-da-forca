d31aee305689d5019cd63c1320f78c9d505a6b8c
LaianeBarreto <117527904+LaianeBarreto@users.noreply.github.com>
2023-05-15 05:06:04 +0000

    Tratamentos de palavras

--- jogo-da-forca.py
+++ jogo-da-forca.py
@@ -76,3 +76,48 @@ estados = {
   5:estado5,
   6:estado6
 }
+
+
+#Etapa 1 - Listas de Palavras
+animais = open("palavras/animais.txt", "r", encoding="utf8").readlines()
+frutas = open("palavras/frutas.txt", "r", encoding="utf8").readlines()
+objetos = open("palavras/objetos.txt", "r", encoding="utf8").readlines()
+paises = open("palavras/paises.txt", "r", encoding="utf8").readlines()
+
+#Temas
+palavras = {
+        'ANIMAL': animais,
+        'FRUTA': frutas,
+        'OBJETO': objetos,
+        'PAÍS': paises
+}
+
+#Tratamento de caracteres especiais
+letrasEspeciais = {
+    "Á":"A", "À":"A", "Ã":"A", "Â":"A",
+    "É":"E", "È":"E", "Ê":"E",
+    "Í":"I", "Ì":"I", "Î":"I",
+    "Ó":"O", "Õ":"O", "Ò":"O",
+    "Ú":"U", "Ù":"U", "Û":"U",
+    "Ç":"C",
+    "-":"",
+    " ":"",
+  }
+
+def convertPalavra(palavra):
+  """
+  palavra --> string contendo uma palavra
+
+  ________________
+  Retorna a palavra sem 'acentos', sem hifens e em CAIXA ALTA.
+  Também tira espaços e quebra de linha da palavra, se houver.
+  """
+  palavra = palavra.replace("\n", "")
+  palavra = palavra.upper()
+  
+  for letra in palavra:
+    if letra in letrasEspeciais:
+      semAcento = letrasEspeciais[letra] #pega a letra sem acento
+      palavra = palavra.replace(letra, semAcento)
+
+  return palavra
